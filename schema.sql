1bef892a3837376f12df788386cdb808bd411946
acepsaepudin <acepsaepudin>
2016-06-29 13:08:32 +0700

    ganti schema sesuai vsd

--- schema.sql
+++ schema.sql
@@ -2,11 +2,11 @@ DROP TABLE IF EXISTS kategori;
 create table kategori(
     id int(11) not null auto_increment,
     nama_kategori varchar(200),
-    karyawan_id_karyawan int(11),
+    id_pengguna int(11),
     primary key(id));
 
-DROP TABLE IF EXISTS karyawan;
-create table karyawan(
+DROP TABLE IF EXISTS pengguna;
+create table pengguna(
     id int(11) not null auto_increment,
     nama varchar(200),
     alamat varchar(200),
@@ -14,7 +14,7 @@ create table karyawan(
     jenis_kelamin int(1),
     email varchar(200),
     password varchar(200),
-    jabatan varchar(200),
+    status varchar(200),
     primary key(id)
 );
 
@@ -26,27 +26,47 @@ create table prasarana(
     longitude varchar(200),
     latitude varchar(200),
     kategori_id_kategori int(11),
-    primary key(id),
-    constraint prasarana_kategori_fk foreign key (kategori_id_kategori) references kategori(id)
-);
-
-DROP TABLE IF EXISTS users;
-create table pelapor(
-    id int(11) not null auto_increment,
-    nama varchar(200),
-    email varchar(200),
-    password varchar(200),
-    token varchar(200),
     primary key(id)
 );
+
 DROP TABLE IF EXISTS kerusakan;
 create table kerusakan(
     id int(11) not null auto_increment,
-    id_pelapor int(11),
     tanggal datetime,
     deskripsi varchar(200),
     foto varchar(200),
     status int(1),
+    id_pengguna int(11),
+    id_prasarana int(11),
+    primary key(id)
+);
+
+DROP TABLE IF EXISTS detail_kerusakan;
+create table detail_kerusakan(
+    id int(11) not null auto_increment,
+    deskripsi varchar(200),
+    id_kerusakan int(11),
+    primary key(id)
+); 
+
+DROP TABLE IF EXISTS perbaikan;
+create table perbaikan(
+    id int(11) not null auto_increment,
+    nama_barang varchar(200),
+    harga varchar(200),
+    status varchar(200),
+    id_kerusakan int(11),
+    primary key(id)
+);
+
+DROP TABLE IF EXISTS detail_perbaikan;
+create table detail_perbaikan(
+    id int(11) not null auto_increment,
+    tanggal datetime,
+    status varchar(200),
+    feedback varchar(200),
+    id_pengguna int(11),
+    id_perbaikan int(11),
     primary key(id)
 );
 
