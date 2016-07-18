f0f8b29250cd3d67b3c74fd0d9198a464ff1b11c
Acep Saepudin <qwertynesia@gmail.com>
2016-07-18 22:58:41 +0700

    save auth inside parent controller

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -8,6 +8,7 @@ class MY_Controller extends CI_Controller
 	function __construct()
 	{
 		parent::__construct();
+		$this->authenticate->check_login();
 		$this->stencil->layout('admin_layout');
 	}
 }
